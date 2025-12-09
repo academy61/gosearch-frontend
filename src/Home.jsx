72ee528576517edaac7f682b5e9a36babc05e194
Jobs <137252184+academy61@users.noreply.github.com>
2025-12-09 19:27:54 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -40,7 +40,7 @@ function Home() {
 
   const handleLogout = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/logout', {
+      const response = await fetch('https://gosearch-backend.onrender.com/api/logout', {
         method: 'POST',
         credentials: 'include'
       });
